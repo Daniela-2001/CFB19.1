e4f0e45c2616dddbd984ec1908e026d41458b601
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-25 08:45:28 +0000

    CFB19.1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace aula19._1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for(int num=0 ;num<10;num++)
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
